ff97b6cff74354b0ac6d399a621b5b61863ea75c
ryanc01 <81178044+ryanc01@users.noreply.github.com>
2024-08-02 19:53:42 -0400

    Removed App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+import Sidenav from "./components/Sidenav";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidenav></Sidenav>
+    </div>
+  );
 }
 
 export default App;
