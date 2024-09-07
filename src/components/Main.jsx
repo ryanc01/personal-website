10caf0632d49e544b46a172df15ac2ca058d4e9f
ryanc01 <81178044+ryanc01@users.noreply.github.com>
2024-09-07 00:43:05 -0400

    Added background image on main

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover"
+        src="https://images.unsplash.com/photo-1486848538113-ce1a4923fbc5?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div>
+          <h1>I'm Ryan Clarke</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
