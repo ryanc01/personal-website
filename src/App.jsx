fe012f5c2431f063b64a5ee776808bfe0cbce889
ryanc01 <81178044+ryanc01@users.noreply.github.com>
2024-09-07 23:59:55 -0400

    Completed Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div>
       <Sidenav />
       <Main />
+      <Experience />
     </div>
   );
 }
